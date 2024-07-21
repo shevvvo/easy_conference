565c5d9207cbf2ad80f1f87fa29a7f4e8bd50f5a
shevchenkov <shevchenkov@trueconf.com>
2024-07-21 21:27:13 +0300

    todo

--- include/primitives/common_json.h
+++ include/primitives/common_json.h
@@ -6,4 +6,6 @@ namespace primitives {
 NetworkMessage deserialize_json(std::string&& message);
 
 std::string serialize_json(NetworkMessage&& message);
+
+//TODO: pack/unpack message with '\n'. State-machine
 } // namespace primitives
