8d4d3b59f1b30b29f9fb7b2703c712c9e167d59c
shevchenkov <shevchenkov@trueconf.com>
2024-07-21 14:52:23 +0300

    string_view

--- primitives/include/user_interaction.h
+++ primitives/include/user_interaction.h
@@ -3,11 +3,11 @@
 
 #include "message.h"
 #include <istream>
-#include <string>
+#include <string_view>
 
 namespace primitives {
 template <typename InputStream, typename OutputStream, typename MessageType>
-void get_user_input(InputStream& in, OutputStream& out, std::string message, MessageType& user_input) {
+void get_user_input(InputStream& in, OutputStream& out, std::string_view message, MessageType& user_input) {
     out << message;
     in >> user_input;
 }
